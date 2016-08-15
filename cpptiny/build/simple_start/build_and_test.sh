25e678b0b744869df73d79150d0f1868b75479f4
dengmingxuan@hotmail.com <dengmingxuan@hotmail.com>
2016-08-15 19:25:09 +0800

    second commit with a simple Hello World

---
+++ cpptiny/build/simple_start/build_and_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+g++ ./cpptiny/cc/simple_start/simple_main.cc -o ./cpptiny/build/simple_start/simple_main
+
+echo "trying to run the test: ------------------"
+
+./cpptiny/build/simple_start/simple_main
+
+
---
+++ cpptiny/cc/simple_start/simple_main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+  cout << "first main function of simple_start" << endl;
+  cout << "Rock said: What are you doing?" << endl;
+  return (0);
+}
